19cc67f62b0f4dcb430e81a45f6b37b9a88991f5
Peter <peter@Pennypacker.(none)>
2011-08-21 14:38:10 -0600

    Adding all activity overrides.  Moving some things around

--- src/com/smpete/frugieLog/FrugieLogActivity.java
+++ src/com/smpete/frugieLog/FrugieLogActivity.java
@@ -32,6 +32,13 @@ public class FrugieLogActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
         
         curDate = new Date();
         setDateText();
@@ -40,7 +47,11 @@ public class FrugieLogActivity extends Activity {
         currentFruit = new Food(FoodType.FRUIT);
         currentVeggie = new Food(FoodType.VEGGIE);
         setDataFromDB(new Date());
-        
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
     }
     @Override
     public void onPause(){
@@ -48,6 +59,17 @@ public class FrugieLogActivity extends Activity {
     	db.open();
     	long result = db.insertStats(curDate, currentFruit.getServingTenths(), currentVeggie.getServingTenths());
     	db.close();
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
     }
     
     private void setDataFromDB(Date date){
