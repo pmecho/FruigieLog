60ccbedb40cc7f6de1a254d24e6cc2e87f967ce7
Peter Elliott <pelliott19@gmail.com>
2011-09-05 21:18:12 -0600

    adding fragment files for main controls

---
+++ src/com/smpete/frugieLog/MainControlFragment.java
@@ -0,0 +1,26 @@
+package com.smpete.frugieLog;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainControlFragment extends Fragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		
+		return null;
+	}
+	
+	@Override
+	public void onPause(){
+		
+	}
+}
